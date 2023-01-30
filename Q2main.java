a2c78906c2538058fa4799c7f91d168a182aa082
RSK SUPRETHA <120301602+RSKSUPRETHA@users.noreply.github.com>
2023-01-30 23:48:09 +0530

    Add files via upload

---
+++ Q2main.java
@@ -0,0 +1,28 @@
+package com.greatlearning.q1.main;
+
+import java.util.Scanner;
+
+public class Q2main {
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner sc= new Scanner(System.in);
+		BalanceBracket checkBalanced = new BalanceBracket();
+	
+		System.out.println("Enter the brackets to check whether they are Balanced Brackets or UnBalanced Brackets :");
+		String str = sc.nextLine();
+		
+		boolean result;
+		result = BalanceBracket.checkBalanced(str);
+		
+		if(result)
+			System.out.println("The entered strings is Balanced brackets");
+		else
+			System.out.println("The entired string do not contain Balanced bracket");
+		
+		sc.close();
+ 
+	}
+
+}
